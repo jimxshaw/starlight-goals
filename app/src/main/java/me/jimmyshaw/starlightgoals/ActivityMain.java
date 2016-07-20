e5cf2d3606dd0a23a842573eab7763564613eece
Jim Shaw <jimxshaw@gmail.com>
2016-07-20 16:42:19 -0500

    Added toolbar layout. Included toolbar layout in main activity layout.

--- app/src/main/java/me/jimmyshaw/starlightgoals/ActivityMain.java
+++ app/src/main/java/me/jimmyshaw/starlightgoals/ActivityMain.java
@@ -1,7 +1,7 @@
 package me.jimmyshaw.starlightgoals;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -9,5 +9,7 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
