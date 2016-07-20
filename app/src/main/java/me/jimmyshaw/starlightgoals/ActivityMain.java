4679ef5572567ccff3fcc314b4c7d6579ec374c4
Jim Shaw <jimxshaw@gmail.com>
2016-07-20 17:48:53 -0500

    Changed the toolbar's theme to show dark text. Created and tested a view on click listener for the add a goal button in main activity.

--- app/src/main/java/me/jimmyshaw/starlightgoals/ActivityMain.java
+++ app/src/main/java/me/jimmyshaw/starlightgoals/ActivityMain.java
@@ -2,14 +2,39 @@ package me.jimmyshaw.starlightgoals;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ActivityMain extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.button_add_a_goal)
+    Button buttonAdd;
+
+    private View.OnClickListener addListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(ActivityMain.this, "Add button was clicked!", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        buttonAdd.setOnClickListener(addListener);
 
     }
 }
