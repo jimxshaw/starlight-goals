1978fd9afeeb43e752ce76641b9675d702068c2c
Jim Shaw <jimxshaw@gmail.com>
2016-07-22 16:40:51 -0500

    Added realm change listener to main activity's onStart and onStop.

--- StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/ActivityMain.java
+++ StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/ActivityMain.java
@@ -5,19 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.realm.Realm;
+import io.realm.RealmChangeListener;
 import io.realm.RealmResults;
 import me.jimmyshaw.starlightgoals.adapters.AdapterGoals;
 import me.jimmyshaw.starlightgoals.models.Goal;
 
 public class ActivityMain extends AppCompatActivity {
 
+    public static final String TAG = "Jim";
+
     Realm realm;
+
+    RealmResults<Goal> realmResults;
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -35,6 +41,18 @@ public class ActivityMain extends AppCompatActivity {
         }
     };
 
+    private RealmChangeListener realmChangeListener = new RealmChangeListener() {
+        @Override
+        public void onChange(Object element) {
+            Log.d(TAG, "onChange: ");
+        }
+    };
+
+    private void showDialogAddAGoal() {
+        DialogAddAGoal dialog = new DialogAddAGoal();
+        dialog.show(getSupportFragmentManager(), "Dialog Add a Goal");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,18 +64,24 @@ public class ActivityMain extends AppCompatActivity {
 
         realm = Realm.getDefaultInstance();
 
-        RealmResults<Goal> realmResults = realm.where(Goal.class).findAllAsync();
+        realmResults = realm.where(Goal.class).findAllAsync();
 
         buttonAdd.setOnClickListener(addListener);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new AdapterGoals(this, realmResults));
 
     }
 
-    private void showDialogAddAGoal() {
-        DialogAddAGoal dialog = new DialogAddAGoal();
-        dialog.show(getSupportFragmentManager(), "Dialog Add a Goal");
+    @Override
+    protected void onStart() {
+        super.onStart();
+        realmResults.addChangeListener(realmChangeListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        realmResults.addChangeListener(realmChangeListener);
     }
 }
