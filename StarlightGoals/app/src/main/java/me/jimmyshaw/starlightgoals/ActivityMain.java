d0d226f4d3e0ec37034b4ff6b4fbdcaa43a6165d
Jim Shaw <jimxshaw@gmail.com>
2016-07-22 14:04:40 -0500

    Added recycler view to gradle.

--- StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/ActivityMain.java
+++ StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/ActivityMain.java
@@ -2,6 +2,7 @@ package me.jimmyshaw.starlightgoals;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,9 @@ public class ActivityMain extends AppCompatActivity {
 
     @BindView(R.id.button_add_a_goal)
     Button buttonAdd;
+
+    @BindView(R.id.recycler_view_goals)
+    RecyclerView recyclerView;
 
     private View.OnClickListener addListener = new View.OnClickListener() {
         @Override
