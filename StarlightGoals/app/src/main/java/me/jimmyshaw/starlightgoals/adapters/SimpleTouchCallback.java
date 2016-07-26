81fc7c98ac454baa7379e3b77a8b07d895ef0f8f
Jim Shaw <jimxshaw@gmail.com>
2016-07-26 14:00:22 -0500

    Added if conditions to SimpleTouchCallback's onSwiped, onChildDraw and onChildDrawOver methods to prevent swiping away non-row items such as the footer or the no items to display layout.

--- StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/adapters/SimpleTouchCallback.java
+++ StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/adapters/SimpleTouchCallback.java
@@ -1,9 +1,13 @@
 package me.jimmyshaw.starlightgoals.adapters;
 
+import android.graphics.Canvas;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 
 public class SimpleTouchCallback extends ItemTouchHelper.Callback {
+
+    public static final String TAG = "Jim";
 
     private SwipeListener swipeListener;
 
@@ -38,6 +42,30 @@ public class SimpleTouchCallback extends ItemTouchHelper.Callback {
 
     @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-        swipeListener.onSwipe(viewHolder.getLayoutPosition());
+        // This method has to communicate with the recycler view adapter in order to delete the row
+        // item that has been swiped.
+        // This if conditional is needed so that only row items can be swiped away. Otherwise, the footer
+        // or the no items layout can be swiped away. But this isn't enough as we have to override
+        // two methods onChildDraw and onChildDrawOver as their default behavior is what governs
+        // the swiping in the x direction or y direction.
+        if (viewHolder instanceof AdapterGoals.GoalHolder) {
+            swipeListener.onSwipe(viewHolder.getLayoutPosition());
+        }
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        Log.d(TAG, "onChildDraw -> dX: " + dX + " dY: " + dY);
+        if (viewHolder instanceof AdapterGoals.GoalHolder) {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    }
+
+    @Override
+    public void onChildDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        Log.d(TAG, "onChildDrawOver -> dX: " + dX + " dY: " + dY);
+        if (viewHolder instanceof AdapterGoals.GoalHolder) {
+            super.onChildDrawOver(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
     }
 }
