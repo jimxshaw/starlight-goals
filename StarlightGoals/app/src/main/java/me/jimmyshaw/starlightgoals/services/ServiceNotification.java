c5d62154406aa66639ba4b4621cdfc147ae31b4b
Jim Shaw <jimxshaw@gmail.com>
2016-07-31 13:02:56 -0500

    Refactored alarm scheduler into a static method in Util class.

--- StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/services/ServiceNotification.java
+++ StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/services/ServiceNotification.java
@@ -1,11 +1,15 @@
 package me.jimmyshaw.starlightgoals.services;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.content.Intent;
 import android.util.Log;
 
+import br.com.goncalves.pugnotification.notification.PugNotification;
 import io.realm.Realm;
 import io.realm.RealmResults;
+import me.jimmyshaw.starlightgoals.ActivityMain;
+import me.jimmyshaw.starlightgoals.R;
 import me.jimmyshaw.starlightgoals.models.Goal;
 
 /**
@@ -38,6 +42,24 @@ public class ServiceNotification extends IntentService {
         }
     }
 
+    private void issueNotification(Goal goal) {
+        String message = getString(R.string.notification_message_small_device)
+                + " \"" + goal.getGoal() + "\"";
+
+        PugNotification.with(this)
+                .load()
+                .title(R.string.app_name)
+                .message(message)
+                .bigTextStyle(message)
+                .smallIcon(R.drawable.ic_star)
+                .largeIcon(R.drawable.ic_polaris)
+                .flags(Notification.DEFAULT_ALL) // The defaults are lights, sound and vibrate.
+                .autoCancel(true) // Clears the message if the user clicks on it.
+                .click(ActivityMain.class) // Clicking on the message will take the user to this app's main activity.
+                .simple()
+                .build();
+    }
+
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.d(TAG, "onHandleIntent:");
@@ -50,7 +72,9 @@ public class ServiceNotification extends IntentService {
             RealmResults<Goal> realmResults = realm.where(Goal.class).equalTo("completed", false).findAll();
 
             for (Goal currentGoal : realmResults) {
-
+                if (isNotificationNeeded(currentGoal.getDateAdded(), currentGoal.getDateDue())) {
+                    issueNotification(currentGoal);
+                }
             }
         }
         finally {
