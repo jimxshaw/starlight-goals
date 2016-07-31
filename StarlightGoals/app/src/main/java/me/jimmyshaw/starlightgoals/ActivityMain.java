2c0796ebefde05ce0b4cad95319dc708568b732e
Jim Shaw <jimxshaw@gmail.com>
2016-07-31 11:46:52 -0500

    Added comments in main activity. Added isNotificationNeeded method in notification service.

--- StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/ActivityMain.java
+++ StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/ActivityMain.java
@@ -212,9 +212,26 @@ public class ActivityMain extends AppCompatActivity {
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleTouchCallback);
         itemTouchHelper.attachToRecyclerView(recyclerView);
 
+        // The vision for using an alarm manager is that we'll be combining it with a service so that
+        // whenever a goal is 90%, 95% or whatever other % finished approaching its due date time then
+        // we'll notify the user. We want our alarm service to check if any goals are approaching their
+        // due date time once every hour.
+        // The reason we're putting our intent inside a pending intent is because we want the alarm
+        // service to be checking due dates regardless if our app is running. We'd even like it to check
+        // when the actual phone is asleep. A service inside a pending intent will run no matter if its
+        // hosting app process is destroyed.
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         Intent intent = new Intent(this, ServiceNotification.class);
+        // The requestCode int, which we arbitrarily put as 100, is an int used to differentiate
+        // this pending intent from other pending intents. The final parameter, FLAG_UPDATE_CURRENT
+        // means that if the described PendingIntent already exists, then keep it but replace its
+        // extra data with what is in this new Intent.
         PendingIntent pendingIntent = PendingIntent.getService(this, 101, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // ELAPSED_REALTIME_WAKEUP will use real time plus will wake up the phone to run whatever is in
+        // the pending intent. Second parameter is the trigger time, after wakeup how long before the
+        // pending intent triggers. Third parameter is the interval time, how often do we want the
+        // pending intent to fire. Note that the interval time is recommended to be an hour or more.
+        // If not then the device's battery will drain quickly.
         alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, 1000, 5000, pendingIntent);
 
     }
--- StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/services/ServiceNotification.java
+++ StarlightGoals/app/src/main/java/me/jimmyshaw/starlightgoals/services/ServiceNotification.java
@@ -4,6 +4,10 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+import io.realm.Realm;
+import io.realm.RealmResults;
+import me.jimmyshaw.starlightgoals.models.Goal;
+
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
@@ -14,12 +18,46 @@ public class ServiceNotification extends IntentService {
 
     public ServiceNotification() {
         super("ServiceNotification");
-        Log.d(TAG, "ServiceNotification");
+        Log.d(TAG, "ServiceNotification:");
+    }
+
+    private boolean isNotificationNeeded(long dateAdded, long dateDue) {
+        long currentTime = System.currentTimeMillis();
+
+        if (currentTime > dateDue) {
+            // If we're already passed a goal's due date then it doesn't make sense to send notifications.
+            return false;
+        }
+        else {
+            // Notify the user when a certain percentage of the due date time has passed.
+            long ninetyPercentDifference = (long) (0.90 * (dateDue - dateAdded));
+
+            // If the current time has passed the time when the goal was added plus the specified
+            // percentage then return true, otherwise return false.
+            return (currentTime > (dateAdded + ninetyPercentDifference)) ? true : false;
+        }
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Log.d(TAG, "onHandleIntent");
+        Log.d(TAG, "onHandleIntent:");
+
+        Realm realm = null;
+        try {
+            realm = Realm.getDefaultInstance();
+            // Query realm and find all our incomplete goals. We don't need to use the findAllAsync
+            // method because we're already in a background thread with this service.
+            RealmResults<Goal> realmResults = realm.where(Goal.class).equalTo("completed", false).findAll();
+
+            for (Goal currentGoal : realmResults) {
+
+            }
+        }
+        finally {
+            if (realm != null) {
+                realm.close();
+            }
+        }
     }
 
 
